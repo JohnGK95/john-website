a239f261e67686ae0d1e9fe5bfb3fb696f0254c8
John Kerlagon <johnkerlagon@gmail.com>
2026-06-14 19:00:18 +0800

    Made a reuseable navbar and applied it across all pages

---
+++ js/navbar.js
@@ -0,0 +1,12 @@
+document.getElementById("navbar").innerHTML = `
+  <nav>
+    <h2>John Kerlagon</h2>
+
+    <div>
+      <a href="/index.html">Home</a>
+      <a href="/languages/index.html">Languages</a>
+      <a href="/projects/index.html">Projects</a>
+      <a href="#contact">Contact</a>
+    </div>
+  </nav>
+`;
